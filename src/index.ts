ccbdfc370bd9ac04e0e319246fc67039201bff28
lopesma11 <ml722090@gmail.com>
2025-07-10 11:45:21 -0300

    feat: getting started with WebSockets Implementation

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,22 @@
 import express from "express";
+import http from "node:http";
 import mongoose from "mongoose";
 import { router } from "./router";
 import path from "node:path";
+import { Server } from "socket.io";
 
 mongoose
   .connect("mongodb://localhost:27017")
   .then(() => {
-    const app = express();
     const port = 3001;
+
+    const app = express();
+    const server = http.createServer(app);
+    const io = new Server(server);
+
+    io.on("connect", () => {
+      console.log("Conectou!");
+    });
 
     app.use((req, res, next) => {
       res.setHeader("Access-Control-Allow-Origin", "*");
@@ -23,7 +32,7 @@ mongoose
     app.use(express.json());
     app.use(router);
 
-    app.listen(port, () => {
+    server.listen(port, () => {
       console.log(`🚀 Server is running on http://localhost:${port}`);
     });
   })
